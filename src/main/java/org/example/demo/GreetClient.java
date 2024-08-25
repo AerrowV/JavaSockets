5b912b8015ab642c4164dc7f70e096f2ca107942
Asim <businessasimk@gmail.com>
2024-08-25 20:26:31 +0200

    Updated

--- src/main/java/org/example/demo/GreetClient.java
+++ src/main/java/org/example/demo/GreetClient.java
@@ -1,4 +1,27 @@
 package org.example.demo;
+import java.net.*;
+import java.io.*;
 
 public class GreetClient {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port) throws IOException {
+        clientSocket = new Socket("127.0.0.1", 6666);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public String sendMessage(String msg) throws IOException {
+        out.println(msg);
+        String resp = in.readLine();
+        return resp;
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+    }
 }
--- src/test/java/GreetServerTest.java
+++ src/test/java/GreetServerTest.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import org.example.demo.GreetClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 
 public class GreetServerTest {
+
+    @Test
+    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
+        GreetClient client = new GreetClient();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("hello server");
+        Assertions.assertEquals("hello client", response);
+    }
 }
